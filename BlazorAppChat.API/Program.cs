77658db6df8eeff3bc64e755289a788886ef5a27
RHamudi <ramonramos.silva16@gmail.com>
2023-03-08 22:04:58 -0300

    Criando Hub SignalR

--- BlazorAppChat.API/Program.cs
+++ BlazorAppChat.API/Program.cs
@@ -1,3 +1,5 @@
+using BlazorAppChat.API.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +11,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.MapHub<ChatHub>("/chat");
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -19,5 +23,12 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors(builder =>
+{
+    builder.AllowAnyOrigin();
+    builder.AllowAnyMethod();
+    builder.AllowAnyHeader();
+});
 
 app.Run();
